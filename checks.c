708f1d68a26e249c2e51886a031d8164fd480db2
Matteo PARIGI <maparigi@e3r7p1.clusters.42paris.fr>
2022-04-06 18:36:28 +0200

    no_dup ✓

--- checks.c
+++ checks.c
@@ -6,7 +6,7 @@
 /*   By: maparigi <maparigi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 18:19:32 by maparigi          #+#    #+#             */
-/*   Updated: 2022/04/06 18:21:51 by maparigi         ###   ########.fr       */
+/*   Updated: 2022/04/06 18:29:48 by maparigi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,15 @@
 int	no_dup(int *a_stack, int n)
 {
 	int	i;
+	int	j;
 
-	i = 0;
+	i = -1;
+	while (++i < n - 1)
+	{
+		j = i;
+		while (++j < n)
+			if (a_stack[i] == a_stack[j])
+				return (0);
+	}
+	return (1);
 }
